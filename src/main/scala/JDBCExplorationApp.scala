27e0bf916a966dc6b4449087b1627fdee8cab151
Luis Da Costa <dacosta.le@gmail.com>
2014-07-16 14:47:23 -0400

    Specification of query for summarizing interactions
    Quick implementation of the brute-force query that summarizes the data
    we expect for Step 0. Beautification to come.

--- src/main/scala/JDBCExplorationApp.scala
+++ src/main/scala/JDBCExplorationApp.scala
@@ -6,6 +6,9 @@ import java.sql.{DriverManager, Connection}
 
 object JDBCExplorationApp {
 
+  /**
+   * Parses parameters from command line and fills up a map with the extracted values.
+   */
   private def parseParams(args: Array[String]): Map[String, String] = {
     def parseNextParam(argsList: List[String], m: Map[String, String]): Map[String, String] = {
       argsList match {
@@ -16,6 +19,32 @@ object JDBCExplorationApp {
     }
     parseNextParam(args.toList, Map[String, String]())
   }
+
+  /**
+   * Builds a query string that returns all activities from RAM.
+   */
+  private def mkQueryStringForActivities(filterSearchesByName: Boolean, dateFilterAsString: String): String = {
+    "(select keyword,activity_date,account_key from raw_activity_metrics WHERE DATE(activity_date) " + dateFilterAsString + ") ram " +
+      "JOIN " +
+      "account a " +
+      "ON " +
+      "a.account_key = ram.account_key JOIN location l " +
+      "ON a.account_location_key = l.location_key " +
+      {
+        if (filterSearchesByName)
+          "WHERE ucase(REPLACE(REPLACE(ram.keyword,\"-\",\"\"),\" \",\"\")) <> ucase(replace(replace(a.account_name, \"'\", \"\"),\" \",\"\")) "
+        else
+          " "
+      } +
+      "GROUP BY a.account_key " +
+      "ORDER BY a.account_name" +
+      ")"
+  }
+
+  /**
+   * We want to filter the RAM table for activity happening following this filter.
+   */
+  private val DATE_FILTER_AS_STRING = "< \"2013-08-01\"" // "> \"2014-03-01\""
 
   def main(args: Array[String]) {
 
@@ -41,14 +70,26 @@ object JDBCExplorationApp {
             println(s"Failure connecting to ${jdbcURL}, user = ${username}, passwd = <provided by the user>. Do 'printStackTrace' if you need more details.")
             None
         }
+      val query = "SELECT " +
+        "tAll.account_key, tAll.account_id, tAll.account_name, tAll.location_city, tAll.location_zip_code, tAll.totalCount, tDiff.perCategoryCount, tDiff.perCategoryCount / tAll.totalCount AS categoryProportion FROM " +
+        "(select a.account_key, a.account_id, a.account_name, l.location_city, l.location_zip_code, COUNT(*) As totalCount " +
+        "FROM " +
+        mkQueryStringForActivities(false, DATE_FILTER_AS_STRING) + " tAll " +
+        "LEFT JOIN " +
+        "(select a.account_key, COUNT(*) As perCategoryCount " +
+        "FROM " +
+        mkQueryStringForActivities(true, DATE_FILTER_AS_STRING) + " tDiff " +
+        "ON tAll.account_key = tDiff.account_key;"
+
+      println(query)
       connOpt.map{ conn =>
         println("OK, we are IN!")
         val stmt = conn.createStatement()
-        val rsSet = stmt.executeQuery("SELECT COUNT(*) FROM account")
+        val rsSet = stmt.executeQuery(query)
         if (rsSet.next())
-          println(s"Table 'account' has ${rsSet.getInt(1)} rows")
+          println(s"Query successfully executed")
         else
-          println("Error fetching row count from table 'account'")
+          println(s"Query execution failed")
         stmt.close()
         conn.close()
       }
