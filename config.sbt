716d4158cc363714c60c4fd634d8bd68ccb9e139
Luis Da Costa <dacosta.le@gmail.com>
2014-07-16 12:33:44 -0400

    Connection to a mysql database

---
+++ config.sbt
@@ -0,0 +1,11 @@
+name := "Simple Project"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.0.0"
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.12"
+
+resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
---
+++ src/main/scala/JDBCExplorationApp.scala
@@ -0,0 +1,51 @@
+/**
+ * JDBC implementation of YPLeads version 0 queries.
+ */
+
+import java.sql.{DriverManager, Connection}
+
+object JDBCExplorationApp {
+
+  private def parseParams(args: Array[String]): Map[String, String] = {
+    def parseNextParam(argsList: List[String], m: Map[String, String]): Map[String, String] = {
+      argsList match {
+        case Nil => m
+        case "--passwd" :: passwdValue :: t => parseNextParam(t, m + ("passwd" -> passwdValue))
+        case param :: t => parseNextParam(t, m)
+      }
+    }
+    parseNextParam(args.toList, Map[String, String]())
+  }
+
+  def main(args: Array[String]) {
+
+    val params: Map[String, String] = parseParams(args)
+    if (!params.contains("passwd")) {
+      println("Usage: --passwd: <passwd>")
+      println("No password specified. Exiting now.")
+    }
+    else {
+      val server = "ibdevro.itops.ad.ypg.com"
+      val portNumber = 5029
+      val dbName = "ypa_dev"
+      val username = "ldcosta1"
+      val passwd = params.get("passwd").get // will not fail. See the 'if' above.
+      val jdbcURL = s"jdbc:mysql://${server}:${portNumber}/${dbName}"
+
+      val connOpt: Option[Connection] =
+        try {
+          Some(DriverManager.getConnection(jdbcURL, username, passwd))
+        }
+        catch {
+          case e: Exception =>
+            println(s"Failure connecting to ${jdbcURL}, user = ${username}, passwd = <provided by the user>. Do 'printStackTrace' if you need more details.")
+            None
+        }
+      connOpt.map{ conn =>
+        println("OK, we are IN!")
+        conn.close()
+      }
+    }
+  }
+
+}
